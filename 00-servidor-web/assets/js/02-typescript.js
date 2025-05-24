56a9fbbcf7a50959203fe1ba45078d7331d2ec0c
Gabo <gabriel.vasconez@epn.edu.ec>
2025-05-24 00:01:28 -0500

    Deber: pagina web clonada

--- 00-servidor-web/assets/js/02-typescript.js
+++ 00-servidor-web/assets/js/02-typescript.js
@@ -42,3 +42,54 @@ console.log(func(1));
 console.log(func(null));
 console.log(func({}));
 console.log(func([]));
+var persona = {
+    "nombre": "Gabriel",
+    'apellido': 'Vasconez',
+    edad: 21,
+    hijos: 0,
+    casado: false,
+    zapatos: undefined,
+    ropa: {
+        color: "negro",
+        talla: 40
+    },
+    mascotas: ['Thomas', 'Hera']
+};
+console.log(persona);
+console.log(persona.nombre);
+console.log(persona['apellido']);
+// Modificacion:
+persona.nombre = "Esteban";
+persona["nombre"] = "Gabriel";
+// Reasignacion:
+// persona = {}; -> ERROR
+// Creacion de nuevos atributos (Solo cuando el objeto esta definido como any):
+persona.carrera = "Software";
+persona.facultad = "FIS";
+// Eliminacion de propiedades:
+persona.facultad = undefined;
+delete persona.facultad;
+// Variables por valor o por referencia
+// Variables por valor:
+// En general, son las primitivas: number, string, boolean
+var edadGabriel = 21;
+var edadDavid = edadGabriel;
+console.log(edadDavid);
+edadGabriel = edadGabriel + 1;
+console.log(edadDavid); // No cambia
+// Variables por referencia:
+// En JavaScript no existen los punteros. 
+// Object: {}, []
+var notas = {
+    total: 10
+};
+var notas2 = notas; // notas2 "apunta" a notas
+notas2.total = notas2.total + 1;
+console.log(notas.total); // 11
+// Clonacion de objetos:
+var notasClonadas = Object.assign({}, notas);
+notasClonadas.total++;
+console.log(notas.total); // 11
+// Clonacion de arreglos:
+var arregloACopiar = [1, 2, 3];
+var arregloClonado = Object.assign([], arregloACopiar);
--- 00-servidor-web/assets/js/02-typescript.ts
+++ 00-servidor-web/assets/js/02-typescript.ts
@@ -54,3 +54,85 @@ console.log(func(1));
 console.log(func(null));
 console.log(func({}));
 console.log(func([]));
+
+const persona:any = {
+    "nombre": "Gabriel",
+    'apellido': 'Vasconez',
+    edad: 21,
+    hijos: 0,
+    casado: false,
+    zapatos: undefined,
+    ropa: {
+        color: "negro",
+        talla: 40
+    },
+    mascotas: ['Thomas', 'Hera']
+}
+
+console.log(persona);
+
+console.log(persona.nombre);
+console.log(persona['apellido']);
+// Modificacion:
+persona.nombre = "Esteban";
+persona["nombre"] = "Gabriel";
+
+// Reasignacion:
+// persona = {}; -> ERROR
+
+// Creacion de nuevos atributos (Solo cuando el objeto esta definido como any):
+persona.carrera = "Software";
+persona.facultad = "FIS";
+
+// Eliminacion de propiedades:
+persona.facultad = undefined;
+delete persona.facultad;
+
+// Variables por valor o por referencia
+// Variables por valor:
+// En general, son las primitivas: number, string, boolean
+let edadGabriel = 21;
+let edadDavid = edadGabriel;
+console.log(edadDavid);
+edadGabriel = edadGabriel + 1;
+console.log(edadDavid); // No cambia
+
+// Variables por referencia:
+// En JavaScript no existen los punteros. 
+// Object: {}, []
+let notas = {
+    total: 10
+};
+
+let notas2 = notas; // notas2 "apunta" a notas
+notas2.total = notas2.total + 1;
+console.log(notas.total); // 11
+// Clonacion de objetos:
+let notasClonadas = Object.assign({}, notas);
+notasClonadas.total++;
+console.log(notas.total) // 11
+// Clonacion de arreglos:
+let arregloACopiar = [1, 2, 3];
+let arregloClonado = Object.assign([], arregloACopiar);
+
+// Arreglos:
+const arregloEjemplo = [1, 2, 3, 4, 5];
+// for of: obtiene valores
+for (let valorDelArreglo of arregloEjemplo) {
+    console.log('Valor:', valorDelArreglo);
+}
+// for in: obtiene indices
+for (let indiceDelArreglo in arregloEjemplo) {
+    console.log('Indice:', indiceDelArreglo);
+    console.log('Valor:', arregloEjemplo[indiceDelArreglo]);
+}
+// Añadir al final un elemento:
+arregloEjemplo.push(6); // [1, 2, 3, 4, 5, 6]
+// Eliminar el ultimo elemento:
+arregloEjemplo.pop(); // [1, 2, 3, 4, 5]
+// Añadir al principio:
+arregloEjemplo.unshift(0); // [0, 1, 2, 3, 4, 5]
+// Eliminar y agregar elementos:
+// splice: indiceInicio, nroDeElementosAEliminar, elementosAAñadir
+arregloEjemplo.splice(0, 3, 3, 4, 5);
+// Resultado: [3, 4, 5, 3, 4, 5]
